c91c5f7c1879c100b19b448839e445421ef7af7b
Junio MM <info@juniomarques.com>
2023-02-28 21:59:34 +0100

    get movies

--- src/pages/MoviesPage.tsx
+++ src/pages/MoviesPage.tsx
@@ -1,3 +1,26 @@
+import { useCallback, useEffect, useState } from 'react'
+import { apiMdb } from '../lib/axios'
+
+interface Movie {
+  average_rating: number
+  backdrop_path: string
+  id: number
+  poster_path: string
+  release_date: string
+  title: string
+}
 export function MoviesPage() {
-  return <h1>Page Movie</h1>
+  const [movies, setMovies] = useState<Movie[]>([])
+  const TMDB_KEY = import.meta.env.VITE_TMDB_KEY
+
+  const fetchMovies = useCallback(async () => {
+    const response = await apiMdb.get(`movie/popular?api_key=${TMDB_KEY}`)
+    setMovies([response.data])
+  }, [TMDB_KEY])
+
+  useEffect(() => {
+    fetchMovies()
+  }, [fetchMovies])
+
+  return <div>Page Movie</div>
 }
