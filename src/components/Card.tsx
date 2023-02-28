1304dfc35578bc65567d184a991ffe56bc5680ee
Junio MM <info@juniomarques.com>
2023-02-28 22:56:02 +0100

    Styled Card Component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,3 +1,5 @@
+import { LinkSimple } from 'phosphor-react'
+import { Link } from 'react-router-dom'
 import { PATH_IMAGES_TMDB } from '../utils/pathImagesTMDB'
 
 interface CardProps {
@@ -9,14 +11,34 @@ interface CardProps {
 
 export function Card(props: CardProps) {
   return (
-    <div>
+    <div className="overflow-hidden rounded-md bg-slate-100 py-2 group transition:transform">
       <header>
-        <h3>{props.title}</h3>
-        <img
-          className="transition:transform, opacity duration-500 ease-in-out group-hover:scale-[1.2] group-hover:opacity-50"
-          src={PATH_IMAGES_TMDB.concat(props.poster_path)}
-          alt={`Image for movie ${props.title}`}
-        />
+        <h3 className="font-bold px-3 text-center relative z-20 transition:transform, opacity duration-700 ease-in-out group-hover:translate-y-36">
+          {props.title}
+        </h3>
+        <Link className="relative" to={`detail/${props.id}`}>
+          {props.poster_path ? (
+            <img
+              className="transition:transform, opacity duration-500 ease-in-out group-hover:scale-[1.2] group-hover:opacity-50"
+              src={PATH_IMAGES_TMDB.concat(props.poster_path)}
+              alt={`Image for movie ${props.title}`}
+            />
+          ) : (
+            <img
+              src="https://via.placeholder.com/328x492?text=image not found"
+              alt="image not found"
+            />
+          )}
+          <span
+            className="absolute top-[40%] flex items-center gap-2
+          left-[50%] translate-x-[-50%] bg-slate-800
+           text-white px-8 py-4 rounded-lg opacity-0 transition:opacity
+            duration-500 ease-in-out group-hover:opacity-100
+            hover:bg-slate-900"
+          >
+            More info <LinkSimple size={20} />
+          </span>
+        </Link>
       </header>
     </div>
   )
