bf71ee1acfd78fb2a27ac586e093010b18a6ce2b
Junio MM <info@juniomarques.com>
2023-03-22 14:34:58 +0100

    myList page

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import { DefaultLayout } from './layout/DefaultLayout'
 import { DetailPage } from './pages/DetailPage'
 import { MoviesPage } from './pages/MoviesPage'
+import { Mylist } from './pages/MyList'
 
 export function Router() {
   return (
@@ -9,6 +10,7 @@ export function Router() {
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<MoviesPage />} />
         <Route path="/detail/:id" element={<DetailPage />} />
+        <Route path="/mylist" element={<Mylist />} />
       </Route>
     </Routes>
   )
---
+++ src/pages/MyList.tsx
@@ -0,0 +1,17 @@
+import { useContext } from 'react'
+import { Card } from '../components/Card'
+import { MoviesContext } from '../contexts/MoviesContext'
+
+export function Mylist() {
+  const { myMovies } = useContext(MoviesContext)
+  return (
+    <div className="max-w-5xl m-auto py-5">
+      <h1 className="text-white mb-4 text-xl font-semibold">My list</h1>
+      <div className="flex flex-col-reverse md:grid md:grid-cols-3 gap-5 pb-10">
+        {myMovies.map((movie) => {
+          return <Card key={movie.id} {...movie} />
+        })}
+      </div>
+    </div>
+  )
+}
